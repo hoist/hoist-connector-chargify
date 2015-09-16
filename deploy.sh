9b14e185ec0efd91ded481e46a71bb1e255e4bce
Owen Evans <owen@bgeek.net>
2015-09-16 15:11:40 +1200

    use base-box

--- deploy.sh
+++ deploy.sh
@@ -6,22 +6,22 @@ TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 
 echo "making timestamped deploy directory";
 
-mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/${TIMESTAMP}
+mkdir -p ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "copying files to deploy directory";
 
-cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/${TIMESTAMP}
+cp -r . ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP}
 
 echo "removing current symlink";
 
-rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/current
+rm -f ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "relinking current symlink";
 
-ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/current
+ln -s ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/${TIMESTAMP} ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/current
 
 echo "removing old deploy directories";
 
-(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-chargify/'{}'
+(ls -t ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/|head -n 5;ls ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/)|sort|uniq -u|xargs -I '{}' rm -r ${ROOT_CONNECTOR_DIR}/hoist-connector-${CONNECTOR_NAME}/'{}'
 
 echo "done!";
